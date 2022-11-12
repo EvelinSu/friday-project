bb39d88d131a4c8783625100999cb73f2c09cf52
HuziMuzi <laif17860@mail.ru>
2022-11-12 14:03:25 +0300

    added isInitialized in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {HashRouter} from "react-router-dom";
 import {SSiteWrapper} from "./ui/layout/styled";
 import Header from "./ui/layout/Header/Header";
@@ -6,8 +6,15 @@ import Pages from "./ui/pages/Pages";
 import {ThemeProvider} from "styled-components";
 import {baseTheme} from "./ui/styles/constants";
 import HeaderLinks from "./temp/HeaderLinks";
+import {useAppDispatch} from "./hooks/hooks";
+import {authMeTC} from "./bll/appReducer";
 
 function App() {
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        dispatch(authMeTC())
+    },[])
 
     return (
         <ThemeProvider theme={baseTheme}>
--- src/bll/appReducer.ts
+++ src/bll/appReducer.ts
@@ -2,12 +2,12 @@ import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 import {authAPI} from "../dal/api";
 import {AxiosError} from "axios";
 import {TAppDispatch} from "./store/store";
+import {setIsLoggedInAC} from "./authReducer";
 
 export type TAuth = {
     isInitialized: boolean
 }
 const initialState: TAuth = {
-
     isInitialized: false
 }
 
@@ -17,21 +17,21 @@ const slice = createSlice({
     initialState: initialState,
     reducers: {
         setIsInitialized(state, action: PayloadAction<{ value: boolean }>) {
-
+            state.isInitialized = action.payload.value
         }
-    }
+    },
+
 })
 
 export const appReducer = slice.reducer
-
 export const  {setIsInitialized} = slice.actions
 
 
 
 export const authMeTC = () => (dispatch: TAppDispatch) => {
     authAPI.authMe()
-        .then(res => {
-            console.log(res)
+        .then(() => {
+            dispatch(setIsLoggedInAC({value: true}))
             dispatch(setIsInitialized({value: true}))
         })
         .catch( (e:AxiosError) => {
