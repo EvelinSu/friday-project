2c60f356a5416f2bdc6078c0000c7f29ec30edb5
HuziMuzi <laif17860@mail.ru>
2022-11-12 12:14:45 +0300

    added loginTC and setIsLoggedInAC

--- src/bll/authReducer.ts
+++ src/bll/authReducer.ts
@@ -1,19 +1,44 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {authAPI, LoginDataType} from "../dal/api";
+import {Dispatch} from "react";
+import {AxiosError} from "axios";
+import {TAppDispatch} from "./store/store";
 
-export type TAuth = {}
-const initialState: TAuth = {}
+export type TAuth = {
+    isLoggedIn: boolean
+}
+const initialState: TAuth = {
+    isLoggedIn: false
+}
 
 
 const slice = createSlice({
     name: 'auth',
     initialState: initialState,
-    reducers: {}
+    reducers: {
+        setIsLoggedInAC (state, action : PayloadAction<{value : boolean}>) {
+            state.isLoggedIn = action.payload.value
+        }
+    }
 })
 
 export const authReducer = slice.reducer
 
-
+export const  {setIsLoggedInAC} = slice.actions
 
 // export type TAuthActions = {}
 
 
+
+
+export const loginTC = (data : LoginDataType) => (dispatch: TAppDispatch) => {
+    authAPI.login(data)
+        .then(res => {
+            console.log(res)
+            dispatch(setIsLoggedInAC({value: true}))
+            // dispatch()
+        })
+        .catch( (e:AxiosError) => {
+            console.log(e)
+        })
+}
