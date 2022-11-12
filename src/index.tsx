950b1751e91a1ba5334efc6d68d10adebad0250c
HuziMuzi <laif17860@mail.ru>
2022-11-12 09:50:14 +0300

    fix import store

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './ui/styles/index.css';
 import App from './App';
 import {Provider} from "react-redux";
-import store from "./bll/store/store";
+import {store} from "./bll/store/store";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
