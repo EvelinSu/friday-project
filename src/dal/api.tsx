9a871833a5357e44e59a9fe820e0ac55f4c22ce7
HuziMuzi <laif17860@mail.ru>
2022-11-14 14:11:22 +0300

    added env file

--- src/dal/api.tsx
+++ src/dal/api.tsx
@@ -1,13 +1,15 @@
 import axios from "axios";
 
 export const instance = axios.create({
-    baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/',
+    baseURL: process.env.REACT_APP_BACK_URL || "http://localhost:7542/2.0/",
     // baseURL: "https://neko-back.herokuapp.com/2.0/",
 
     // baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:7542/2.0/' :
     // 'https://neko-back.herokuapp.com/2.0/',
     withCredentials: true,
 });
+
+console.log(process.env);
 
 export const authAPI = {
     login(data: LoginDataType) {
