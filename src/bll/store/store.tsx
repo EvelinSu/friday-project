69261606ef398b4f99472434a20f8f8c7d50f65a
HuziMuzi <laif17860@mail.ru>
2022-11-16 13:39:11 +0300

    added new types on Dispatch

--- src/bll/store/store.tsx
+++ src/bll/store/store.tsx
@@ -1,4 +1,4 @@
-import thunkMiddleware from "redux-thunk";
+import thunk, { ThunkDispatch } from "redux-thunk";
 import { configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "../authReducer";
 import { appReducer } from "../appReducer";
@@ -6,6 +6,7 @@ import { registerReducer } from "../registerReducer";
 import { forgotPassReducer } from "../forgotPassReducer";
 import { packsReducer } from "../packsReducer";
 import { packsParamsReducer } from "../packsParamsReducer";
+import { AppActionsTypes } from "../../common/types/ActionsTypes";
 
 export const store = configureStore({
     reducer: {
@@ -16,13 +17,13 @@ export const store = configureStore({
         registration: registerReducer,
         forgotPass: forgotPassReducer,
     },
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().prepend(thunkMiddleware),
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(thunk),
 });
 
 export type TRootState = ReturnType<typeof store.getState>;
 
-export type TAppDispatch = typeof store.dispatch;
+// export type TAppDispatch = typeof store.dispatch;
+export type TAppDispatch = ThunkDispatch<TRootState, unknown, AppActionsTypes>;
 
 // @ts-ignore
 window.store = store;
