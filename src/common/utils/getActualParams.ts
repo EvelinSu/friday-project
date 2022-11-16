0a4045ccb07f0b7cddcbda1756b1b6056bbcb690
EvelinSu <firest-evi@mail.ru>
2022-11-16 21:58:27 +0300

    - fixed grid adaptive

--- src/common/utils/getActualParams.ts
+++ src/common/utils/getActualParams.ts
@@ -2,7 +2,7 @@ import { TPacksParams } from "../../dal/ResponseTypes";
 
 export const getActualPacksParams = (searchParams: URLSearchParams): TPacksParams => {
     const windowHeight = window.innerHeight;
-    const pageCount = windowHeight > 1080 ? 20 : windowHeight > 790 ? 16 : 12;
+    const pageCount = windowHeight > 1080 ? 20 : windowHeight > 930 ? 16 : 12;
     return {
         user_id: searchParams.get("user_id") || undefined,
         packName: searchParams.get("packName") || undefined,
