0fd29e542380dc59a1d763dbb163a3329c7cb095
HuziMuzi <laif17860@mail.ru>
2022-11-17 12:42:00 +0300

    fix bag on redirect packs list

--- src/ui/pages/PacksList/PacksPage.tsx
+++ src/ui/pages/PacksList/PacksPage.tsx
@@ -2,20 +2,21 @@ import React, { useEffect, useMemo } from "react";
 import { SPageWrapper } from "../styled";
 import { useAppDispatch, useAppSelector } from "../../../hooks/hooks";
 import Pagination from "../../components/Pagination/Pagination";
-import { useSearchParams } from "react-router-dom";
+import { Navigate, useSearchParams } from "react-router-dom";
 import LoaderIcon from "../../assets/loaders/loader";
 import { setCardParams } from "../../../bll/packsParamsReducer";
 import { getActualPacksParams } from "../../../common/utils/getActualParams";
 import { loadPacks } from "../../../bll/packsReducer";
 import PacksList from "./PacksList";
 import PacksPagePanel from "./PacksPagePanel/PacksPagePanel";
+import { PATH } from "../Pages";
 
 const PacksPage = () => {
     const [searchParams] = useSearchParams();
     const dispatch = useAppDispatch();
 
     const { cardPacksData } = useAppSelector((state) => state.packs);
-    const { isFetching } = useAppSelector((state) => state.auth);
+    const { isFetching, isLoggedIn } = useAppSelector((state) => state.auth);
     const { cardPacks } = useAppSelector((state) => state.packs.cardPacksData);
 
     const stateParams = useAppSelector((state) => state.packsParams);
@@ -30,6 +31,8 @@ const PacksPage = () => {
     useEffect(() => {
         dispatch(loadPacks(URLParams));
     }, [URLParams, dispatch]);
+
+    if (!isLoggedIn) return <Navigate to={PATH.signIn} />;
 
     return (
         <SPageWrapper>
