e5effc39b02a1c9b5d5047bae1b468ab1c6392d8
HuziMuzi <laif17860@mail.ru>
2022-11-20 17:32:05 +0300

    search test

--- src/ui/pages/PacksPage/PacksPage.tsx
+++ src/ui/pages/PacksPage/PacksPage.tsx
@@ -9,6 +9,7 @@ import { loadPacks } from "../../../bll/packsReducer";
 import PacksList from "./PacksList";
 import PacksPagePanel from "./PacksPagePanel/PacksPagePanel";
 import PacksNotFound from "./PacksNotFound/PacksNotFound";
+import { setCardParams } from "../../../bll/packsParamsReducer";
 
 const PacksPage = () => {
     const dispatch = useAppDispatch();
@@ -19,6 +20,13 @@ const PacksPage = () => {
     const { cardPacksData } = useAppSelector((state) => state.packs);
     const { isFetching } = useAppSelector((state) => state.app);
     const { cardPacks } = useAppSelector((state) => state.packs.cardPacksData);
+    const packsParams = useAppSelector((state) => state.packsParams);
+
+    // это не удалять
+    useEffect(() => {
+        if (JSON.stringify(packsParams) !== JSON.stringify(URLParams))
+            dispatch(setCardParams(URLParams));
+    }, [dispatch, URLParams]);
 
     useEffect(() => {
         dispatch(loadPacks(URLParams));
