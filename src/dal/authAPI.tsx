a16b8e6791e618b08fc84a013d21a02d649dac0a
Kirill <apatski94@gmail.com>
2022-11-24 23:38:06 +0300

    fix message on email to recovery password

--- src/dal/authAPI.tsx
+++ src/dal/authAPI.tsx
@@ -1,4 +1,4 @@
-import { instance } from "./instance";
+import {instance} from "./instance";
 import {
     TLoginData,
     TProfileData,
@@ -8,7 +8,7 @@ import {
     TResponseUserData,
     TSendPassData,
 } from "./ResponseTypes";
-import { AxiosResponse } from "axios";
+import {AxiosResponse} from "axios";
 
 export const authAPI = {
     login(data: TLoginData) {
@@ -33,10 +33,15 @@ export const authAPI = {
         return instance.post<{ email: string }, AxiosResponse<TResponseSendEmail>>("auth/forgot", {
             email: email,
             from: "app Cards",
-            message: `<div style="background-color: #3b3b49; color: white; padding: 15px">
-                password recovery link: 
-                <a  href="https://evelinsu.github.io/friday-project/#/login/changePassword/$token$"  style=" color: deepskyblue">
-                link</a></div>`,
+            message: `<div style="text-align: center; background-color: #fff; color: black;">
+                        <p style="font-size: 26px; font-weight: 600; text-align: center; color: #4F659E;">Need to reset your password?</p>
+                        <p style="opacity: 0.5; line-height: 24px; text-align: center;">Click on the button</p>
+                        <div style="text-align: center">
+                        <a href="https://evelinsu.github.io/friday-project/#/login/changePassword/$token$" style=" color: #fff; display: inline-block; padding: 10px 30px;  border-radius: 20px; background-color: #7398CE;  text-align: center; text-decoration: none; font-size: 16px; letter-spacing: 1px;">
+                            Reset your password</a>
+                        </div>
+                        <p style="opacity: 0.5; line-height: 24px; text-align: center;">If you did not forget your password, you can ignore this email.</p>
+                       </div>`,
         });
     },
     sendNewPass(data: TSendPassData) {
