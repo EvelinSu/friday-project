bd7b1c8d6872079612bdab47fb368705365f7e03
HuziMuzi <laif17860@mail.ru>
2022-11-24 10:25:38 +0300

    added dragon drop on load img in modal

--- src/common/utils/handlerErrors.ts
+++ src/common/utils/handlerErrors.ts
@@ -5,12 +5,19 @@ import { setAppMessage } from "../../bll/appReducer";
 export const handlerErrors = (dispatch: Dispatch, e: unknown) => {
     const err = e as Error | AxiosError;
     if (axios.isAxiosError(err)) {
-        console.log(err);
-
-        const errorMessage = err.response?.data
-            ? err.response.data
-            : err.message + ", more details in the console";
-        dispatch(setAppMessage({ text: errorMessage, severity: "error" }));
+        if (err.request.status !== 413) {
+            const errorMessage = err.response?.data
+                ? err.response.data
+                : err.message + ", more details in the console";
+            dispatch(setAppMessage({ text: errorMessage, severity: "error" }));
+        } else {
+            dispatch(
+                setAppMessage({
+                    text: "The image was not uploaded.Try other images ",
+                    severity: "error",
+                })
+            );
+        }
         return;
     }
     const errNetwork = `${err.message}, more details in the console`;
--- src/ui/components/FileUploadArea/FileUploadArea.tsx
+++ src/ui/components/FileUploadArea/FileUploadArea.tsx
@@ -1,41 +1,29 @@
-import React, {ChangeEvent, FC, useRef} from 'react';
-import {SFileUploadArea, SFileUploadAreaImage} from "./styled";
-import {SText} from "../Text/SText";
-import {useAppDispatch} from "../../../hooks/hooks";
-import {setAppMessage} from "../../../bll/appReducer";
+import React, { ChangeEvent, FC, useRef, useState } from "react";
+import { SFileUploadArea, SFileUploadAreaImage } from "./styled";
+import { SText } from "../Text/SText";
+import { useAppDispatch } from "../../../hooks/hooks";
+import { setAppMessage } from "../../../bll/appReducer";
 
 type TFileUploadAreaProps = {
-    placeholder?: string
-    onClick?: (file: string) => void
-    name?: string
-    onChange: (file: string) => void
-    value: string
-    size?: number
-    fileType?: 'image' | 'document'
-}
-export const FileUploadArea: FC<TFileUploadAreaProps> = ({size = 4000000, ...props}) => {
+    placeholder?: string;
+    onClick?: (file: string) => void;
+    name?: string;
+    onChange: (file: string) => void;
+    value: string;
+    size?: number;
+    fileType?: "image" | "document";
+};
+export const FileUploadArea: FC<TFileUploadAreaProps> = ({ size = 4000000, ...props }) => {
     const inputAccept = {
-        "image": '.png, .jpg, .jpeg, .gif',
-        "document": ".doc, .pdf"
-    }
-    const dispatch = useAppDispatch()
+        image: ".png, .jpg, .jpeg, .gif",
+        document: ".doc, .pdf",
+    };
+    const dispatch = useAppDispatch();
     const inputFile = useRef<HTMLInputElement | null>(null);
+    const [drag, setDrag] = useState(false);
 
     const onClickHandler = () => {
         inputFile.current?.click();
-    }
-
-    const uploadHandler = (e: ChangeEvent<HTMLInputElement>): void => {
-        if (e.target.files && e.target.files.length) {
-            const file = e.target.files[0];
-            if (file.size < size) {
-                convertFileToBase64(file, (file64: string) => {
-                    props.onChange(file64);
-                });
-            } else {
-                dispatch(setAppMessage({severity: "error", text: "The file size should be less than 4 megabytes"}))
-            }
-        }
     };
 
     const convertFileToBase64 = (file: File, callBack: (value: string) => void): void => {
@@ -46,9 +34,52 @@ export const FileUploadArea: FC<TFileUploadAreaProps> = ({size = 4000000, ...pro
         };
         reader.readAsDataURL(file);
     };
+    const uploadFile = (files: any) => {
+        if (files && files.length) {
+            const file = files[0];
+            if (file.size < size) {
+                convertFileToBase64(file, (file64: string) => {
+                    props.onChange(file64);
+                });
+            } else {
+                dispatch(
+                    setAppMessage({
+                        severity: "error",
+                        text: "The file size should be less than 4 megabytes",
+                    })
+                );
+            }
+        }
+    };
+    const uploadHandler = (e: ChangeEvent<HTMLInputElement>): void => {
+        const files = e.target.files;
+        uploadFile(files);
+    };
+    const dragStartHandler = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        setDrag(true);
+    };
+
+    const dragLeaveHandler = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        setDrag(false);
+    };
+
+    const onDropHandler = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        setDrag(false);
+        const files = e.dataTransfer.files;
+        uploadFile(files);
+    };
 
     return (
-        <SFileUploadArea onClick={onClickHandler}>
+        <SFileUploadArea
+            onClick={onClickHandler}
+            onDragStart={(e) => dragStartHandler(e)}
+            onDragLeave={(e) => dragLeaveHandler(e)}
+            onDragOver={(e) => dragStartHandler(e)}
+            onDrop={(e) => onDropHandler(e)}
+        >
             <SFileUploadAreaImage img={props.value}></SFileUploadAreaImage>
             <input
                 accept={props.fileType && inputAccept[props.fileType]}
@@ -56,13 +87,15 @@ export const FileUploadArea: FC<TFileUploadAreaProps> = ({size = 4000000, ...pro
                 type="file"
                 ref={inputFile}
                 onChange={uploadHandler}
-                style={{display: "none"}}
+                style={{ display: "none" }}
             />
             <SText opacity={0.4}>
-                {props.placeholder
-                || (!props.value || (props.value === "null")) ? "Choose file" : "Change file"}
+                {!drag
+                    ? props.placeholder || !props.value || props.value === "null"
+                        ? "Choose file or drag it here"
+                        : "Change file or drag it here"
+                    : "release"}
             </SText>
         </SFileUploadArea>
     );
 };
-
