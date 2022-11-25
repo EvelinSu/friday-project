f97b9dd33d0d1815c1bf03f3665cb964d14914e9
HuziMuzi <laif17860@mail.ru>
2022-11-25 23:16:21 +0300

    added select on grades

---
+++ src/ui/pages/LearningPage/Grades.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import SelectGrades from "../../components/SelectGrades/SelectGrades";
+
+const grades = [
+    { id: "1", value: 1, label: "Did not know" },
+    { id: "2", value: 2, label: "Forgot" },
+    { id: "3", value: 3, label: "A lot of thought" },
+    { id: "4", value: 4, label: "Confused" },
+    { id: "5", value: 5, label: "Knew the answer" },
+];
+
+export type grade = {
+    id: string;
+    value: number;
+    label: string;
+};
+
+type TGradesProps = {
+    setGrade: (value: number) => void;
+    grade: number;
+};
+
+export const Grades = ({ setGrade, grade }: TGradesProps) => {
+    const onChangeOption = (value: string) => {
+        setGrade(+value);
+    };
+
+    return <SelectGrades onChangeOption={onChangeOption} grades={grades} value={grade} />;
+};
